078886f095dcfffbf46c6839d1ca26093cde6005
jc fernandezz <jcfernandezz@gmail.com>
2018-11-07 19:59:06 -0400

    Corrige chunks, scripts, wix e id de producto para gp 2016 y 2018

--- GP2018/WinAppSetup/ar04/Release/60 - GRNT GXPR_FC_ELECTRONICA.sql
+++ GP2018/WinAppSetup/ar04/Release/60 - GRNT GXPR_FC_ELECTRONICA.sql
@@ -1,11 +1,7 @@
-GO
-
 grant select on GXPR_FC_ELECTRONICA to gpexp_facturaelectronica;
 
-gO
-
 GO
 
---grant select on GXPR_FC_ELECTRONICA_EXP to gpexp_facturaelectronica;
+grant select on GXPR_FC_ELECTRONICA_EXP to gpexp_facturaelectronica;
 
 GO
